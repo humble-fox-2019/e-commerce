f6e96ad97140406b9016b4bacf08afc46ef936d1
candra saputra <candrasaputra@live.com>
2019-09-16 20:59:50 +0700

    resgister testing done

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,25 +1,30 @@
 function errorHandler(err, req, res, next) {
-    console.log(err);
+    // console.log(err);
 
     if (err.name == 'JsonWebTokenError') {
         res.status(401).json({
-            message: "invalid token"
+            message: ["invalid token"]
         });
     } else if (err.name == "ValidationError") {
+        let msgValidation = [];
+        for (r in err.errors) {
+            msgValidation.push(err.errors[r].message);
+        }
+
         res.status(400).json({
-            message: err.message
+            message: msgValidation
         });
     } else if (err.name == 'CastError') {
         return res.status(400).send({
-            message: 'Invalid ID'
+            message: ['Invalid ID']
         });
     } else if (err.statusCode == 404 && err.msg === undefined) {
         res.status(404).json({
-            message: "Data not found"
+            message: ["Data not found"]
         });
     } else {
         res.status(err.statusCode || 500).json({
-            message: err.msg || 'Internal server error'
+            message: [err.msg] || ['Internal server error']
         });
     }
 }
--- test/tivestore.test.js
+++ test/tivestore.test.js
@@ -5,7 +5,7 @@ const chaiHttp = require('chai-http');
 const app = require('../app');
 
 chai.use(chaiHttp);
-let should = chai.should();
+let expect = chai.expect;
 
 describe('Authentication', function () {
     before(function (done) {
@@ -17,17 +17,41 @@ describe('Authentication', function () {
     });
 
     describe('registration', function () {
-        it('should registration without error', function (done) {
+        it('Registration without error', function (done) {
             chai.request(app)
                 .post('/registration')
                 .send({ name: 'candra saputra', email: 'candrasaputra@live.com', password: 'password123' })
                 .end(function (req, res, next) {
-                    res.body.should.have.property('name');
-                    res.body.should.have.property('email');
-                    res.body.should.have.property('role');
-                    res.body.name.should.equal('candra saputra');
-                    res.body.role.should.equal('customer');
-                    res.should.have.status(201);
+                    expect(res.body).have.property('name');
+                    expect(res.body).have.property('email');
+                    expect(res.body).have.property('role');
+                    expect(res.body.name).equal('candra saputra');
+                    expect(res.body.role).equal('customer');
+                    expect(res).have.status(201);
+                    done();
+                })
+        });
+
+        it('Error email and password format', function (done) {
+            chai.request(app)
+                .post('/registration')
+                .send({ name: 'candra saputra', email: 'candrasaputra@live.com', password: 'password123' })
+                .end(function (req, res, next) {
+                    console.log(res.body.message);
+                    expect(res.body.message).to.have.members(['Email has been used']);
+                    res.should.have.status(400);
+                    done();
+                })
+        });
+
+        it('Error email and password format', function (done) {
+            chai.request(app)
+                .post('/registration')
+                .send({ name: 'candra saputra', email: 'candrasaputra', password: 'password' })
+                .end(function (req, res, next) {
+                    console.log(res.body.message);
+                    expect(res.body.message).to.have.members(['Invalid Email', 'Password must contain at least one letter, one number, and minimum six characters']);
+                    res.should.have.status(400);
                     done();
                 })
         });

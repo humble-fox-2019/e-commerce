fc95a9d2829dbbdb839b2d88a3c7b450312f6ae8
igun369 <guntoro.gyk@gmail.com>
2019-09-16 18:56:37 +0700

    added user

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const UserController = require('../controllers/user');
 
-router.post('/users/signup', UserController.signup);
-router.post('/users/signin', UserController.signin);
+router.post('/signup', UserController.signup);
+router.post('/signin', UserController.signin);
 
 module.exports = router;

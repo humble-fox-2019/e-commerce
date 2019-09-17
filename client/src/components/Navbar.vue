e3792628f0ba7d6fc43877bc75fbfef9e2a2d2cc
candra saputra <candrasaputra@live.com>
2019-09-18 01:03:41 +0700

    header

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav>
+        <v-toolbar drak color="success white--text">
+            <v-toolbar-title class="text-uppercase ">
+                <span>Tive</span>
+                <span class="font-wight-black">store</span>
+            </v-toolbar-title>
+            
+            <v-spacer></v-spacer>
+            
+            <v-btn text>
+                <v-icon left>mdi-home</v-icon>
+                <span>Home</span>
+            </v-btn>
+            
+            <v-btn text class="ml-2">
+                <v-icon left>mdi-basket</v-icon>
+                <span>Cart</span>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+             drawer: false
+        }
+    }
+};
+</script>
+
+<style>
+</style>

e522113414d29dc91ec8a851faa8967b7ae3d3d8
candra saputra <candrasaputra@live.com>
2019-09-18 02:08:23 +0700

    card

--- client/src/views/Cart.vue
+++ client/src/views/Cart.vue
@@ -1,23 +1,78 @@
 <template>
   <div class="cart">
-      <h1 class="subheading grey--text">Cart</h1>
-       
-       <v-container class="my-5">
-           
-           <v-layout row>
-                <v-flex></v-flex>   
-           </v-layout>
-           
-       </v-container>
+    <v-container class="my-5">
+      <h2 class="subheading grey--text">Cart</h2>
+      <v-row>
+        <v-col cols="12" sm="12">
+          <v-simple-table :dense="dense" :fixed-header="fixedHeader" :height="height">
+            <thead>
+              <tr>
+                <th class="text-left">Product</th>
+                <th class="text-center">Qty</th>
+                <th class="text-center">Subtotal</th>
+                <th class="text-center">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in products" :key="item.name">
+                <td>{{ item.name }}</td>
+                <td class="text-right">{{ item.qty }}</td>
+                <td class="text-right">{{ item.subtotal }}</td>
+                <td class="text-center">
+                  BTN
+                </td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th colspan=2 class="text-right">TOTAL</th>
+                <th class="text-right">99900000</th>
+                <th></th>
+              </tr>
+            </tfoot>
+          </v-simple-table>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Cart'
-}
+  name: "Cart",
+  data() {
+    return {
+      products: [
+        {
+          name: 'Frozen Yogurt',
+          qty: 159,
+          subtotal: 400000
+        },
+        {
+          name: 'Ice cream sandwich',
+          qty: 237,
+          subtotal: 550000
+        },
+        {
+          name: 'Eclair',
+          qty: 262,
+          subtotal: 602000
+        },
+        {
+          name: 'Cupcake',
+          qty: 305,
+          subtotal: 203000
+        },
+        {
+          name: 'Gingerbread',
+          qty: 356,
+          subtotal: 330000
+        }
+      ]
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>

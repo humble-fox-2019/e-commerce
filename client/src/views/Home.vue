92404f22fd4051900e0f6f629f926aaf357527a9
Tigor <tigor.hutasuhut@gmail.com>
2019-09-20 21:37:59 +0700

    update

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,34 @@
 <template>
-  <HelloWorld />
+  <v-parallax
+    dark
+    src="https://makingmusicmag.com/wp-content/uploads/2018/10/bass-guitar-1841186_1280.jpg"
+    height="700"
+  >
+    <v-row align="center" justify="end">
+      <h1 class="display-4 mr-12 glow">Bass-Line</h1>
+    </v-row>
+    <v-row align="center" justify="end">
+      <h4 :elevation="10" class="display-3 mr-12 text-right glow">
+        FULLFILL YOUR DREAMS
+        <br />SLAPP THE BASS
+      </h4>
+    </v-row>
+    <v-row align="center" justify="end">
+      <div class="mr-12">
+        <v-btn color="yellow lighten-3" outlined x-large dark>
+          <router-link class="white--text" to="/shop">Shop Now</router-link>
+        </v-btn>
+      </div>
+    </v-row>
+  </v-parallax>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
-export default {
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
+
+<style scoped>
+.glow {
+  text-shadow: 0px 0px 5px #f1a800 !important;
+}
+</style>

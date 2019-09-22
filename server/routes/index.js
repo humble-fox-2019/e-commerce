a987a79b61ca2634e45641301076900a4e638a06
igun369 <guntoro.gyk@gmail.com>
2019-09-22 20:36:16 +0700

    added base route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,12 @@ const userRouter = require('./user');
 const productRouter = require('./product');
 const transactionRouter = require('./transaction');
 
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'connected'
+    })
+});
+
 router.use('/users', userRouter);
 router.use('/products', productRouter);
 router.use('/transactions', transactionRouter);

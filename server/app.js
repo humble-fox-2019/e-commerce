beca32a4a9ab05d26668f362ba39bdb12e227351
whyt <will.heryanto@gmail.com>
2019-09-23 08:39:31 +0700

    final update testing

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ const router = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// mongoose.connect(`mongodb://localhost:27017/e-commerce${(process.env.NODE_ENV === 'test') ? '-test' : ''}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
-mongoose.connect(process.env.ATLAS_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+mongoose.connect(`mongodb://localhost:27017/e-commerce${(process.env.NODE_ENV === 'test') ? '-test' : ''}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+// mongoose.connect(process.env.ATLAS_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
   .then(_ => {
     console.log('Database: connected')
   })
--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,5 +1,4 @@
 module.exports = (err, req, res, next) => {
-  console.log(err)
   let status, errors
   if (err.name === 'JsonWebTokenError') {
     status = 400

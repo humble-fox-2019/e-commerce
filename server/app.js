2c0c7fe80c915173b787f21aceade9bac62942a5
fransnesa <Fransnesah@gmail.com>
2019-09-25 11:28:06 +0700

    fransnesa deploy

--- server/app.js
+++ server/app.js
@@ -8,8 +8,10 @@ const port = process.env.PORT || 3000;
 const routes = require('./routes/index');
 const cors = require('cors')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 
 app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use("/",routes);
